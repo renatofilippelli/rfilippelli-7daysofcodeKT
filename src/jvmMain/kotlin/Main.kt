fb5cedb5cb5fafe9808dc098470a1b40a1bd6454
Renato Filippelli <renatofilippelli@gmail.com>
2023-04-19 21:07:21 -0300

    Segudo dia / Sete dias - 7DaysofCodeKT

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,4 +1,3 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.width
@@ -6,21 +5,27 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.skia.Image
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
+import java.net.URL
+import javax.imageio.ImageIO
 
 @Composable
-@Preview
 fun App() {
     MaterialTheme {
+        val imageUrl = "https://br.web.img3.acsta.net/r_1920_1080/medias/nmedia/18/63/93/01/18948378.jpg"
         Column {
             Text("WALL-E")
             Image(
-                painter = painterResource("images\\img.png"),
+                bitmap = imageUrl.loadImageBitmap(),
                 contentDescription = "WALL-E",
-                modifier = Modifier.width(150.dp)
+                modifier = Modifier.width(200.dp)
             )
             Text("Nota : 8.4 - Ano : 2008")
         }
@@ -31,4 +36,17 @@ fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "IMDB") {
         App()
     }
+}
+
+fun String.loadImageBitmap(): ImageBitmap {
+    val inputStream = BufferedInputStream(
+        URL(this).openStream()
+    )
+    val bufferedImage = ImageIO.read(inputStream)
+
+    val stream = ByteArrayOutputStream()
+    ImageIO.write(bufferedImage, "png", stream)
+    val byteArray = stream.toByteArray()
+
+    return Image.makeFromEncoded(byteArray).toComposeImageBitmap()
 }
