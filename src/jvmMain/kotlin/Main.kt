de68fede4debe75249013f0b3122233b1c5d39a4
Renato Filippelli <renatofilippelli@gmail.com>
2023-04-21 12:41:46 -0300

    Quarto dia / Sete Dias - 7DaysOfCode-Kotlin

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
@@ -14,76 +16,113 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import extensions.loadImageBitmap
+import models.Movie
 
 @Composable
 fun App() {
-
-    val imageUrl = "https://br.web.img3.acsta.net/r_1920_1080/medias/nmedia/18/63/93/01/18948378.jpg"
 
     MaterialTheme (
         colors = darkColors()
     ){
         Surface {
-            Box() {
-                Column(
-                    modifier = Modifier
-                        .width(200.dp)
-                        .padding(10.dp)
-                ) {
-                    Image(
-                        bitmap = imageUrl.loadImageBitmap(),
-                        contentDescription = "WALL-E",
-                        modifier = Modifier
-                            .width(200.dp)
+            Box(modifier = Modifier.fillMaxSize()) {
+                val movies = listOf(
+                    Movie(
+                        titulo = "The Shawshank Redemption",
+                        imagem = "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
+                        nota = 9.2,
+                        ano = 1994
+                    ),
+                    Movie(
+                        titulo = "The Godfather",
+                        imagem = "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,1,128,176_AL_.jpg",
+                        nota = 9.2,
+                        ano = 1972
+                    ),
+                    Movie(
+                        titulo = "The Dark Knight",
+                        imagem = "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX128_CR0,3,128,176_AL_.jpg",
+                        nota = 9.0,
+                        ano = 2008
+                    ),
+                    Movie(
+                        titulo = "Wall-E",
+                        imagem = "https://br.web.img3.acsta.net/r_1920_1080/medias/nmedia/18/63/93/01/18948378.jpg",
+                        nota = 8.4,
+                        ano = 2008
                     )
-                    Row(
-                        modifier = Modifier.fillMaxWidth()
-                            .padding(
-                                top = 8.dp,
-                                start = 8.dp,
-                                end = 8.dp
-                            ),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Icon(
-                                Icons.Default.Star,
-                                "ícone de estrela para nota",
-                                tint = Color.Yellow,
-                                modifier = Modifier.height(16.dp)
-                            )
-                            Text(
-                                text = "8.4",
-                                modifier = Modifier
-                                    .padding(start = 2.dp),
-                                color = Color(0xffeeeeee),
-                                fontSize = 14.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        }
-                        Text(
-                            text = "2008",
-                            fontSize = 14.sp,
-                            color = Color(0xffeeeeee),
+                )
+                LazyColumn {
+                    items(movies) { movie ->
+                        MovieItem(
+                            movie
                         )
                     }
-                    Text(
-                        text = "            WALL-E",
-                        modifier = Modifier.padding(
-                            start = 16.dp,
-                            top = 8.dp,
-                            end = 16.dp
-                        ),
-                        fontSize = 12.sp,
-                        textAlign = TextAlign.Center
-                    )
                 }
             }
         }
     }
 }
 
+
+
+@Composable
+private fun MovieItem(movie: Movie){
+    Column(
+        modifier = Modifier
+            .width(200.dp)
+            .padding(12.dp)
+    ) {
+        Image(
+            bitmap = movie.imagem.loadImageBitmap(),
+            contentDescription = movie.titulo,
+            modifier = Modifier
+                .width(200.dp)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(
+                    top = 8.dp,
+                    start = 8.dp,
+                    end = 8.dp
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    Icons.Default.Star,
+                    "ícone de estrela para nota",
+                    tint = Color.Yellow,
+                    modifier = Modifier.height(12.dp)
+                )
+                Text(
+                    text = movie.nota.toString(),
+                    modifier = Modifier
+                        .padding(start = 2.dp),
+                    color = Color(0xffeeeeee),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Text(
+                text = movie.ano.toString(),
+                fontSize = 14.sp,
+                color = Color(0xffeeeeee),
+            )
+        }
+        Text(
+            movie.titulo,
+            modifier = Modifier.padding(
+                start = 16.dp,
+                top = 8.dp,
+                end = 16.dp
+            ),
+            fontSize = 12.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "IMDB") {
         App()
---
+++ src/jvmMain/kotlin/models/Movie.kt
@@ -0,0 +1,8 @@
+package models
+
+data class Movie(
+    val titulo: String,
+    val imagem: String,
+    val nota: Double,
+    val ano: Int
+)
